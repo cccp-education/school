1f15969b9d635614057258bc32c6fcfb69481863
cheroliv <cheroliv@laposte.net>
2024-09-07 19:52:55 +0200

    push works

--- school/buildSrc/src/main/kotlin/workspace/ai/AssistantPlugin.kt
+++ school/buildSrc/src/main/kotlin/workspace/ai/AssistantPlugin.kt
@@ -1,14 +1,46 @@
 package workspace.ai
 
+import arrow.core.Either
+import arrow.core.getOrElse
+import dev.langchain4j.data.message.AiMessage
+import dev.langchain4j.model.StreamingResponseHandler
+import dev.langchain4j.model.chat.StreamingChatLanguageModel
 import dev.langchain4j.model.ollama.OllamaChatModel
+import dev.langchain4j.model.ollama.OllamaStreamingChatModel
 import dev.langchain4j.model.openai.OpenAiChatModel
+import dev.langchain4j.model.output.Response
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.suspendCancellableCoroutine
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import workspace.ai.AssistantManager.apiKey
 import java.time.Duration.ofSeconds
+import kotlin.coroutines.resume
 
 
 class AssistantPlugin : Plugin<Project> {
+
+    private suspend fun generateStreamingResponse(
+        model: StreamingChatLanguageModel,
+        promptMessage: String
+    ): Either<Throwable, Response<AiMessage>> = Either.catch {
+        suspendCancellableCoroutine { continuation ->
+            model.generate(promptMessage, object : StreamingResponseHandler<AiMessage> {
+                override fun onNext(token: String) {
+                    print(token)
+                }
+
+                override fun onComplete(response: Response<AiMessage>) {
+                    continuation.resume(response)
+                }
+
+                override fun onError(error: Throwable) {
+                    continuation.resume(Either.Left(error).getOrElse { throw it })
+                }
+            })
+        }
+    }
+
     companion object {
         val prompt = """config```--lang=fr;```. 
                             | Salut je suis ${System.getProperty("user.name")}, 
@@ -24,29 +56,9 @@ class AssistantPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
         project.run {
-
-            task("displayAIPrompt") {
-                group = "school-ai"
-                description = "Dislpay on console AI prompt assistant"
-                doFirst { prompt.let(::println) }
-            }
-
-
-            task("helloAssistant") {
-                group = "school-ai"
-                description = "Greetings from AI assistant!"
-                doFirst { println("Hello from assistant") }
-            }
-
-            task("displayOpenAIKey") {
-                group = "school-ai"
-                description = "Display the open ai api keys stored in private.properties"
-                doFirst { println("apiKey : ${project.apiKey}") }
-            }
-
             task("helloOllama") {
                 group = "school-ai"
-                description = "Display the open ai chatgpt hello prompt request."
+                description = "Display the ollama chatgpt prompt request."
                 doFirst {
                     OllamaChatModel
                         .builder()
@@ -59,59 +71,45 @@ class AssistantPlugin : Plugin<Project> {
                         .build()
                         .run { generate(prompt).let(::println) }
                 }
-                doLast {
-                    /*import dev.langchain4j.data.message.AiMessage;
-import dev.langchain4j.model.StreamingResponseHandler;
-import dev.langchain4j.model.chat.StreamingChatLanguageModel;
-import dev.langchain4j.model.ollama.OllamaStreamingChatModel;
-import dev.langchain4j.model.output.Response;
-import org.testcontainers.ollama.OllamaContainer;
-import org.testcontainers.utility.DockerImageName;
+            }
 
-import java.util.concurrent.CompletableFuture;
+            task("helloOllamaStream") {
+                group = "school-ai"
+                description = "Display the ollama chatgpt stream prompt request."
+                doFirst {
+                    runBlocking {
+                        OllamaStreamingChatModel
+                            .builder()
+                            .baseUrl("http://localhost:11434")
+                            .modelName("phi3.5")
+                            .temperature(0.8)
+                            .timeout(ofSeconds(6000))
+                            .logRequests(true)
+                            .logResponses(true)
+                            .build()
+                            .run {
+                                when (val answer = generateStreamingResponse(this, prompt)) {
+                                    is Either.Right ->
+                                        "\nComplete response received: ${answer.value.content().text()}".run(::println)
 
-public class OllamaStreamingChatExample {
-
-  static String MODEL_NAME = "orca-mini"; // try "mistral", "llama2", "codellama" or "phi"
-  static String DOCKER_IMAGE_NAME = "langchain4j/ollama-" + MODEL_NAME + ":latest";
-
-  static OllamaContainer ollama = new OllamaContainer(
-          DockerImageName.parse(DOCKER_IMAGE_NAME).asCompatibleSubstituteFor("ollama/ollama"));
-
-  public static void main(String[] args) {
-    ollama.start();
-    StreamingChatLanguageModel model = OllamaStreamingChatModel.builder()
-        .baseUrl(String.format("http://%s:%d", ollama.getHost(), ollama.getMappedPort(PORT)))
-        .modelName(MODEL_NAME)
-        .temperature(0.0)
-        .build();
-
-    String userMessage = "Write a 100-word poem about Java and AI";
-
-    CompletableFuture<Response<AiMessage>> futureResponse = new CompletableFuture<>();
-    model.generate(userMessage, new StreamingResponseHandler<AiMessage>() {
-
-      @Override
-      public void onNext(String token) {
-        System.out.print(token);
-      }
-
-      @Override
-      public void onComplete(Response<AiMessage> response) {
-        futureResponse.complete(response);
-      }
-
-      @Override
-      public void onError(Throwable error) {
-        futureResponse.completeExceptionally(error);
-      }
-    });
-
-    futureResponse.join();
-    ollama.stop();
-  }
-}*/
+                                    is Either.Left ->
+                                        "\nError during response generation: ${answer.value}".run(::println)
+                                }
+                            }
+                    }
                 }
+            }
+
+            task("displayAIPrompt") {
+                group = "school-ai"
+                description = "Dislpay on console AI prompt assistant"
+                doFirst { prompt.let(::println) }
+            }
+
+            task("displayOpenAIKey") {
+                group = "school-ai"
+                description = "Display the open ai api keys stored in private.properties"
+                doFirst { println("apiKey : ${project.apiKey}") }
             }
 
             task("helloOpenAi") {
@@ -121,7 +119,7 @@ public class OllamaStreamingChatExample {
                     OpenAiChatModel
                         .withApiKey(apiKey)
                         .generate("Say 'Hello World'")
-                        .apply(::println)
+                        .run(::println)
                 }
             }
         }
