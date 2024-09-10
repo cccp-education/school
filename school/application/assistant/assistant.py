afa77b3d8bd8658b8c9c60a10a8a62d751d2af54
cheroliv <cheroliv@laposte.net>
2024-09-10 12:10:04 +0200

    +"Add dataset loading"

--- school/application/assistant/assistant.py
+++ school/application/assistant/assistant.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from datasets import load_dataset
+
+dataset = load_dataset("imdb")
+
 if __name__ == '__main__':
     for placeholder in [
         "foo", "bar", "baz", "qux", "quux",
