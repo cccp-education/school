6014f08d23bdb82a810205ff6e76bf940c755b50
cheroliv <cheroliv@laposte.net>
2024-09-10 13:38:35 +0200

    Refactor: Improve environment setup in tests (#1725967851453)
    - Remove redundant comments and unused code.
    - Ensure each key from ASSISTANT_ENV is present with the correct value after calling set_environment().
    
      Changes:
    
      - Updated test method annotations for clarity.
    
      - Simplified environment check by using a single list comprehension instead of multiple map and contains functions, improving readability while preserving functionality.

--- school/application/assistant/test_assisstant.py
+++ school/application/assistant/test_assisstant.py
@@ -27,19 +27,21 @@ class AssistantTestCase(TestCase):
     #     for key, value in ASSISTANT_ENV.items():
     #         assert_that(os.environ).contains_key(key)
     #         assert_that(os.environ[key]).is_equal_to(value)
-    #
-    # @staticmethod
-    # def test_environment_contains_assistant_env_after_set_environment_functionnal():
-    #     # Vérifier que aucune clé de ASSISTANT_ENV est présente dans os.environ avant set_environment
-    #     assert_that(list(map(lambda key: key in os.environ,
-    #                          ASSISTANT_ENV.keys()))
-    #                 ).is_equal_to([False] * len(ASSISTANT_ENV))
-    #
-    #     set_environment()
-    #     # Vérifier que chaque clé de ASSISTANT_ENV est présente dans os.environ avec la valeur correcte
-    #     assert_that(list(map(lambda key: os.environ[key] == ASSISTANT_ENV[key],
-    #                          ASSISTANT_ENV.keys()))
-    #                 ).is_equal_to([True] * len(ASSISTANT_ENV))
+
+
+    @staticmethod
+    def test_environment_contains_assistant_env_after_set_environment_functionnal():
+        # Vérifier que aucune clé de ASSISTANT_ENV est présente dans os.environ avant set_environment
+        assert_that(list(map(lambda key: key in os.environ,
+                             ASSISTANT_ENV.keys()))
+                    ).is_equal_to([False] * len(ASSISTANT_ENV))
+
+        set_environment()
+        # Vérifier que chaque clé de ASSISTANT_ENV est présente dans os.environ avec la valeur correcte
+        assert_that(list(map(lambda key: os.environ[key] == ASSISTANT_ENV[key],
+                             ASSISTANT_ENV.keys()))
+                    ).is_equal_to([True] * len(ASSISTANT_ENV))
+
 
     @staticmethod
     def test_openapi_chat_connection():
