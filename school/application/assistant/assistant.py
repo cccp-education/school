3399b3bd6e4480d194c192f46669cd3b74c97703
cheroliv <cheroliv@laposte.net>
2024-09-10 06:05:55 +0200

    Added placeholder list prints in main module (assistant.py)

--- school/application/assistant/assistant.py
+++ school/application/assistant/assistant.py
@@ -1,1 +1,8 @@
 # -*- coding: utf-8 -*-
+
+if __name__ == '__main__':
+    for placeholder in [
+        "foo", "bar", "baz", "qux", "quux",
+        "corge", "grault", "garply", "waldo",
+        "fred", "plugh", "xyzzy", "thud"
+    ]: print(placeholder.title())
