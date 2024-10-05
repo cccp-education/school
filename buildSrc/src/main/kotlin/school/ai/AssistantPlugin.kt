cb3a6b2ae6d4083071c24e963acf820ad95d28db
cheroliv <cheroliv@laposte.net>
2024-10-06 01:31:42 +0200

    Push works

--- buildSrc/src/main/kotlin/school/ai/AssistantPlugin.kt
+++ buildSrc/src/main/kotlin/school/ai/AssistantPlugin.kt
@@ -3,14 +3,11 @@ package school.ai
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import school.ai.AssistantManager.createChatTasks
-import school.ai.AssistantManager.openAIapiKey
 import school.ai.AssistantManager.userMessage
-import school.workspace.WorkspaceManager.privateProps
 
 class AssistantPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
-
         project.task("displayE3POPrompt") {
             group = "school-ai"
             description = "Dislpay on console AI prompt assistant"
