2f4bbbc397ffad30e6ccb944ce81fe40f05989e3
cheroliv <cheroliv@laposte.net>
2024-10-10 16:44:57 +0200

    Push works

--- buildSrc/src/main/kotlin/school/translate/TranslatorManager.kt
+++ buildSrc/src/main/kotlin/school/translate/TranslatorManager.kt
@@ -1,9 +1,31 @@
 package school.translate
 
+import school.translate.TranslatorManager.PromptManager.translateMessage
+import school.translate.TranslatorManager.PromptManager.userLanguage
 import school.workspace.WorkspaceUtils.uppercaseFirstChar
 import java.util.*
 
 object TranslatorManager {
+    @JvmStatic
+    fun main(args: Array<String>) = translatorSupportedLanguages
+        .translationTasks()
+        .forEach(::println)
+        .run {
+            userLanguage.run { "userLanguage : $this" }.run(::println)
+            translateMessage.run { "translateMessage : $this" }.run(::println)
+        }.run {
+
+            val currentLocale = Locale.getDefault()
+            currentLocale.run { "currentLocale : $this" }.run(::println)
+            // Force English language for display
+            Locale.setDefault(Locale.ENGLISH)
+
+
+            // Get the display name of the English locale
+            val englishDisplayName = Locale.ENGLISH.getDisplayLanguage(Locale.ENGLISH)
+            println("English display name: $englishDisplayName")
+        }
+
     @JvmStatic
     val translatorSupportedLanguages: Set<String> = setOf(
         Locale.FRENCH,
@@ -12,21 +34,23 @@ object TranslatorManager {
         Locale.of("es")
     ).map { it.language }.toSet()
 
-
     @JvmStatic
     fun Set<String>.translationTasks(): Set<String> = mutableSetOf<String>().apply {
-        this@translationTasks
-            .map { it.uppercaseFirstChar }
-            .forEach { from ->
+        val map = this@translationTasks.map { it.uppercaseFirstChar }
+        map.run {
+            forEach { from ->
                 "translate${from}To".run {
-                    this@translationTasks
-                        .filter { to -> to != from }
+                    map.filter { to -> to != from }
                         .forEach { add("$this${it}") }
                 }
             }
+        }
     }
 
-    @JvmStatic
-    fun main(args: Array<String>) = translatorSupportedLanguages.translationTasks().forEach(::println)
 
+    object PromptManager {
+        val userLanguage = System.getProperty("user.language")!!
+        val translateMessage = """Translate this sentence from $userLanguage to english : 
+            | """.trimMargin()
+    }
 }
