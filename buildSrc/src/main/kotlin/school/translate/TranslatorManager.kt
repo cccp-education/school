cb3316770b6be6bb2a4c557514419fc728a98a26
cheroliv <cheroliv@laposte.net>
2024-10-11 16:09:39 +0200

    Push works

--- buildSrc/src/main/kotlin/school/translate/TranslatorManager.kt
+++ buildSrc/src/main/kotlin/school/translate/TranslatorManager.kt
@@ -12,11 +12,13 @@ import org.gradle.api.Project
 import school.ai.AssistantManager.createOllamaChatModel
 import school.ai.AssistantManager.createOllamaStreamingChatModel
 import school.ai.AssistantManager.generateStreamingResponse
-import school.translate.TranslatorManager.PromptManager.translateMessage
+import school.translate.TranslatorManager.PromptManager.getTranslatePromptMessage
 import school.translate.TranslatorManager.PromptManager.userLanguage
 import school.workspace.WorkspaceUtils.uppercaseFirstChar
 import java.util.*
 import java.util.Locale.*
+
+typealias TranslationTasks = Set<Pair<String/*taskName*/, Pair<String/*from*/, String/*to*/>>>
 
 object TranslatorManager {
     const val MODEL = "aya:8b"
@@ -25,19 +27,22 @@ object TranslatorManager {
     fun main(args: Array<String>) {
         supportedLanguages
             .translationTasks()
-            .forEach(::println)
-            .run {
-                userLanguage.run { "userLanguage : $this" }.run(::println)
-                translateMessage.run { "translateMessage : $this" }.run(::println)
-            }.run {
-                val currentLocale = Locale.getDefault()
-                currentLocale.run { "currentLocale : $this" }.run(::println)
-                // Force English language for display
-                Locale.setDefault(ENGLISH)
-                // Get the display name of the English locale
-                val englishDisplayName = ENGLISH.getDisplayLanguage(ENGLISH)
-                println("English display name: $englishDisplayName")
+//            .apply { forEach(::println) }
+            .let {
+//                userLanguage.run { "userLanguage : $this" }.run(::println)
+                "Gradle task name : ${it.first().first}".run(::println)
+                it.first().second.getTranslatePromptMessage("SALUT LES AMIS COMMENT CA VA ?")
+                    .run { "translateMessage : $this" }.run(::println)
             }
+//            .run {
+//                val currentLocale = Locale.getDefault()
+//                currentLocale.run { "currentLocale : $this" }.run(::println)
+                // Force English language for display
+//                Locale.setDefault(ENGLISH)
+                // Get the display name of the English locale
+//                val englishDisplayName = ENGLISH.getDisplayLanguage(ENGLISH)
+//                println("English display name: $englishDisplayName")
+//            }
     }
 
     @JvmStatic
@@ -48,28 +53,31 @@ object TranslatorManager {
     ).map { it.language }.toSet()
 
     @JvmStatic
-    fun Set<String>.translationTasks(): Set<String> = mutableSetOf<String>().apply {
+    fun Set<String>.translationTasks()
+            : TranslationTasks = mutableSetOf<Pair<String, Pair<String, String>>>().apply {
         this@translationTasks
             .map { it.uppercaseFirstChar }
             .run langNames@{
                 forEach { from: String ->
-                    "translate${from}To".run {
-                        this@langNames.filter {
-                            it != from
-                        }.forEach { add("$this$it") }
+                    "${from}To".run task@{
+                        this@langNames
+                            .filter { it != from }
+                            .forEach { add("${this@task}$it" to (from to it)) }
                     }
                 }
             }
     }
 
-
     object PromptManager {
         val userLanguage = System.getProperty("user.language")!!
         val fromLanguage = System.getProperty("user.language")!!
-        val toLanguage: String = ENGLISH.toLanguageTag()
-        val text = ""
-        val translateMessage = """Translate this sentence from $fromLanguage to $toLanguage : 
-            $text""".trimMargin()
+        fun Pair<String, String>.getTranslatePromptMessage(text: String): String =
+            """Translate this sentence from ${forLanguageTag(first).getDisplayLanguage(ENGLISH).lowercase()} to ${
+                forLanguageTag(second).getDisplayLanguage(
+                    ENGLISH
+                ).lowercase()
+            } : 
+$text""".trimMargin()
     }
 
 
@@ -78,47 +86,49 @@ object TranslatorManager {
     fun Project.createTranslationTasks() = supportedLanguages
         .translationTasks()
         .forEach {
-            createTranslationChatTask(MODEL, "translate${it}")
-            createStreamingTranslationChatTask(MODEL, "translateStream${it}")
+            val text = """ICI LE TEXT A TRADUIRE!"""
+            createTranslationChatTask(MODEL, Triple(it.first, it.second, text))
+            createStreamingTranslationChatTask(MODEL, Triple(it.first, it.second, text))
         }
 
     // Generic function for chat model tasks
-    fun Project.createTranslationChatTask(model: String, taskName: String) {
-        task(taskName) {
+    fun Project.createTranslationChatTask(model: String, taskComponent: Triple<String, Pair<String, String>, String>) {
+        task(taskComponent.first) {
             group = "translator"
             description = "Translate using the Ollama $model chatgpt prompt request."
             doFirst {
-                //TODO : add content mechanism to capture text, from gradle tasks input
-                val text = ""
-                project.runTranslationChat(model, translateMessage.run { "$this : \n$text" })
+                project.runTranslationChat(
+                    model,
+                    taskComponent.second.getTranslatePromptMessage(taskComponent.third)
+                )
             }
         }
     }
 
     // Generic function for streaming chat model tasks
-    fun Project.createStreamingTranslationChatTask(model: String, taskName: String) {
-        task(taskName) {
+    fun Project.createStreamingTranslationChatTask(
+        model: String,
+        taskComponent: Triple<String, Pair<String, String>, String>
+    ) {
+        task(taskComponent.first) {
             group = "translator"
             description = "Translate the Ollama $model chatgpt stream prompt request."
             doFirst {
-                val text = ""
-                runStreamTranslationChat(model, translateMessage.run {
-                    //TODO : add content to the prompt, from gradle tasks input
-                    "$this : \n$text"
-                })
+                project.runStreamTranslationChat(
+                    model,
+                    taskComponent.second.getTranslatePromptMessage(taskComponent.third)
+                )
+
             }
         }
     }
 
-    fun Project.runTranslationChat(model: String, text: String) = createOllamaChatModel(model = model)
-        .run { generate(text).let(::println) }
+    fun Project.runTranslationChat(model: String, text: String) =
+        createOllamaChatModel(model = model).run { generate(text).let(::println) }
 
     fun Project.runStreamTranslationChat(model: String, text: String) = runBlocking {
         createOllamaStreamingChatModel(model).run {
-            when (val answer = generateStreamingResponse(this, translateMessage.run {
-                //TODO : add content to the prompt, from gradle tasks input
-                "$this : \n$text"
-            })) {
+            when (val answer = generateStreamingResponse(this, text)) {
                 is Right -> "Complete response received:\n${
                     answer.value.content().text()
                 }".run(::println)
