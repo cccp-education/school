c58a08d135f76f085ffadc841140ef28e8acfe1f
cheroliv <cheroliv@laposte.net>
2024-10-16 22:02:05 +0200

    Push works

--- buildSrc/src/test/kotlin/school/PluginTests.kt
+++ buildSrc/src/test/kotlin/school/PluginTests.kt
@@ -6,6 +6,7 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.junit.jupiter.api.assertDoesNotThrow
 import school.GradleTestUtils.captureOutput
+import school.GradleTestUtils.displayWorkspaceDataSchemaStructure
 import school.GradleTestUtils.displayWorkspaceStructure
 import school.GradleTestUtils.initWorkspace
 import school.GradleTestUtils.projectInstance
