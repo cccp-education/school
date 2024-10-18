50524b176fd50ead10fb94ac9fdc410125efbfdf
cheroliv <cheroliv@laposte.net>
2024-10-18 03:30:39 +0200

    Push works

--- pyschool/api.py
+++ pyschool/api.py
@@ -10,21 +10,9 @@ from fastapi import FastAPI
 
 api = FastAPI()
 
-
-@api.get("/")
-async def read_root():
-    return {"Hello": "World"}
-
-
-@api.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@api.get("/signup/{signup_id}")
-async def read_item(signup_id: int, q: Union[str, None] = None):
+@api.post("/users/signup/{signup}")
+async def signup(signup_id: int, q: Union[str, None] = None):
     return {"signup_id": signup_id, "q": q}
-
 
 if __name__ == "__main__":
     uvicorn.run('api:api', reload=True, port=8888)
