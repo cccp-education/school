514586892440f340012ff42459e76559c9f14d53
cheroliv <cheroliv@laposte.net>
2024-10-21 03:30:12 +0200

    Push works

--- api/src/test/resources/database/playground_school.sql
+++ api/src/test/resources/database/playground_school.sql
@@ -3,4 +3,16 @@ select count(*) from "user";
 select * from "user";
 select count(*) from user_authority;
 
-select u.id, ua.role from "user" as u , user_authority as ua;
+SELECT
+    u.*,
+    array_agg(DISTINCT a.role) AS user_roles
+FROM
+    "user" u
+        LEFT JOIN
+    user_authority ua ON u.id = ua.user_id
+        LEFT JOIN
+    authority a ON ua.role = a.role
+WHERE
+    lower(u.email) = lower('user@acme.com') OR lower(u.login) = lower('user@acme.com')
+GROUP BY
+    u.id, u.email, u.login;
