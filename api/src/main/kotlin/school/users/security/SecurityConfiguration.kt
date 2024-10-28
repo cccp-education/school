a2e6219607a0a004a60835d127c04df908d88813
cheroliv <cheroliv@laposte.net>
2024-10-28 02:33:15 +0100

    Push works

--- api/src/main/kotlin/school/users/security/SecurityConfiguration.kt
+++ api/src/main/kotlin/school/users/security/SecurityConfiguration.kt
@@ -1,5 +1,7 @@
 package school.users.security
 
+import jakarta.validation.Validator
+import kotlinx.coroutines.reactor.mono
 import org.springframework.beans.factory.getBean
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
@@ -12,7 +14,11 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder.AUTHENTICATION
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder.HTTP_BASIC
 import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity.http
+import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.server.SecurityWebFilterChain
@@ -22,19 +28,62 @@ import org.springframework.security.web.server.header.ReferrerPolicyServerHttpHe
 import org.springframework.security.web.server.util.matcher.NegatedServerWebExchangeMatcher
 import org.springframework.security.web.server.util.matcher.OrServerWebExchangeMatcher
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers.pathMatchers
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.cors.reactive.CorsWebFilter
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource
 import org.springframework.web.server.WebFilter
+import reactor.core.publisher.Mono
 import school.base.http.Web.SpaWebFilter
 import school.base.utils.Log.d
 import school.base.utils.Properties
 import school.base.utils.ROLE_ADMIN
+import school.users.User
+import school.users.User.UserDao.Dao.findOneWithAuths
+import school.users.User.UserDao.Fields.EMAIL_FIELD
+import school.users.User.UserDao.Fields.LOGIN_FIELD
+import org.springframework.security.core.userdetails.User as UserSecurity
 
 
 @Configuration
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
-class SecurityConfiguration(val context: ApplicationContext) {
+class SecurityConfiguration(private val context: ApplicationContext) {
+
+    @Component("userDetailsService")
+    class DomainUserDetailsService(private val context: ApplicationContext) : ReactiveUserDetailsService {
+
+        @Transactional(readOnly = true)
+        @Throws(UsernameNotFoundException::class)
+        override fun findByUsername(emailOrLogin: String): Mono<UserDetails> = context
+            .getBean<Validator>()
+            .run {
+                when {
+                    validateProperty(
+                        User(email = emailOrLogin),
+                        EMAIL_FIELD
+                    ).isNotEmpty() && validateProperty(
+                        User(login = emailOrLogin),
+                        LOGIN_FIELD
+                    ).isNotEmpty() -> throw UsernameNotFoundException("User $emailOrLogin was not found")
+
+                    else -> mono {
+                        context.findOneWithAuths<User>(emailOrLogin).map {
+                            return@mono createSpringSecurityUser(emailOrLogin, it)
+                        }.getOrNull() ?: throw UsernameNotFoundException("User $emailOrLogin was not found")
+                    }
+                }
+            }
+
+        //        @Throws(UserNotActivatedException::class)
+        fun createSpringSecurityUser(lowercaseLogin: String, user: User): UserSecurity = /*when {
+        !user.activated -> throw UserNotActivatedException("User $lowercaseLogin was not activated")
+        else -> */UserSecurity(
+            user.login,
+            user.password,
+            user.roles.map { SimpleGrantedAuthority(it.id) }
+        )
+    }
 
     @Bean("passwordEncoder")
     fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
@@ -45,11 +94,20 @@ class SecurityConfiguration(val context: ApplicationContext) {
             .run(::UserDetailsRepositoryReactiveAuthenticationManager)
             .apply { setPasswordEncoder(passwordEncoder()) }
 
+
+//    @Bean
+//    fun signupSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+//        return http.apply {
+//            authorizeExchange() {
+//                pathMatchers("/api/users/signup")
+//            }
+//        }.build()
+//    }
+
     @Suppress("removal")
     @Bean
-    fun springSecurityFilterChain(
-        http: ServerHttpSecurity
-    ): SecurityWebFilterChain =
+    fun springSecurityFilterChain(http: ServerHttpSecurity)
+            : SecurityWebFilterChain =
         http.securityMatcher(
             NegatedServerWebExchangeMatcher(
                 OrServerWebExchangeMatcher(
