a3e75fddcebbd91e3006989498e806c9d70951d7
cheroliv <cheroliv@laposte.net>
2024-12-11 15:56:58 +0100

    Push works.

--- api/src/main/kotlin/users/signup/UserActivation.kt
+++ api/src/main/kotlin/users/signup/UserActivation.kt
@@ -1,15 +1,16 @@
 package users.signup
 
 import jakarta.validation.constraints.Size
-import users.security.SecurityUtils
+import users.security.SecurityUtils.generateActivationKey
 import java.time.Instant
+import java.time.Instant.now
 import java.util.*
 
 @JvmRecord
 data class UserActivation(
     val id: UUID,
     @field:Size(max = 20)
-    val activationKey: String = SecurityUtils.generateActivationKey,
-    val createdDate: Instant = Instant.now(),
+    val activationKey: String = generateActivationKey,
+    val createdDate: Instant = now(),
     val activationDate: Instant? = null,
 )
