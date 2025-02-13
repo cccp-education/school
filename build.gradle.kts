76665956dd7711ed11f4f43e0a495a2f7d74e262
cheroliv <cheroliv@laposte.net>
2025-02-13 05:44:23 +0100

    feat: Enhance Gradle build configuration for improved project management
    This commit introduces several enhancements to the `build.gradle.kts` file, focusing on improved project management, execution, and clarity:
    
    -   **Centralized Project Configuration:** Added a `School` object to define constants like `GROUP_KEY`, `VERSION_KEY`, `SERVER`, and `CLI` for better organization and reusability.
    -   **Unified Artifact Management:** Introduced `purchaseArtifact()` in an all projects block to ensure that all subprojects have the same group and version, pulled from project properties.
    - **Renamed task**: the task `testApi` is renamed to `reportTestApi`
    - **Refactored task**: the tasks are moved inside the task scope `run`.
    - **Changed some command line arguments**: the command line arguments are changed to `-q`, `-s` for most of the task.
    -   **Enhanced Task Execution:**
        -   Modified `testApi` task to run the check goal and set the `rerun-tasks` goal.
        -   Created `runApi`, `runLocalApi`, `runCli` and `runInstaller` tasks for easily starting the API server in different configurations.
    -   **Improved Wrapper Configuration:** Configured the Gradle wrapper to use version 8.6.
    -   **JVM Arguments**: The JVM arguments are set in all `JavaExec` tasks.
    - **Task `runInstaller`**: The command line is improved by directly calling the jar.
    - **Task `runApi`**: The command line is improved by directly calling the jar.
    - **Task `runLocalApi`**: The command line is improved by directly calling the jar. It also added a profile activation.
    - **Updated task `runCli`**:  The task is enhanced by adding `-q` and `-s` flags to the execution of the cli task.
    - **Added task `testApi`**: Added the `testApi` task that runs the `check` goal.
    - **Added task `reportTestApi`**: Added the `reportTestApi` task that runs the `apiCheckFirefox` goal.
    These changes improve the project's build process, making it more consistent, easier to manage, and faster to use.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,64 +1,102 @@
-import org.gradle.api.tasks.wrapper.Wrapper.DistributionType.BIN
-import school.workspace.WorkspaceUtils.purchaseArtifact
-
 plugins { idea }
+
 apply<school.frontend.SchoolPlugin>()
 apply<school.forms.FormPlugin>()
 apply<school.jbake.JBakeGhPagesPlugin>()
 apply<school.ai.AssistantPlugin>()
 apply<school.translate.TranslatorPlugin>()
 
-purchaseArtifact()
-
-tasks.wrapper {
-    gradleVersion = "8.6"
-    distributionType = BIN
+object School {
+    const val GROUP_KEY = "artifact.group"
+    const val VERSION_KEY = "artifact.version"
+    const val SERVER = "app.Server"
+    const val CLI = "app.CommandLine"
+    const val SQL_SCHEMA = "app.users.api.dao.DatabaseConfiguration"
+    const val SPRING_PROFILE_KEY = "spring.profiles.active"
+    const val MOCKITO_AGENT = "mockito-agent"
+    const val KOTLIN_COMPILER_OPTION_JSR305 = "-Xjsr305=strict"
+    const val MAIN_FUNCTION = "main"
+    const val NODE_MODULES = "node_modules"
 }
 
-tasks.withType<JavaExec> {
-    jvmArgs = listOf(
-        "--add-modules=jdk.incubator.vector",
-        "--enable-native-access=ALL-UNNAMED",
-        "--enable-preview"
-    )
+allprojects {
+    fun Project.purchaseArtifact() = (School.GROUP_KEY to School.VERSION_KEY).run {
+        group = properties[first].toString()
+        version = properties[second].toString()
+    }
+    purchaseArtifact()
 }
 
-tasks.register<Exec>("testApi") {
-    group = "api"
-    commandLine(
-           "./gradlew",
-        "-p",
-        "../api",
-        ":apiCheckFirefox"
-    )
-}
-
-tasks.register<Exec>("runApi") {
-    group = "api"
-    commandLine(
-        "./gradlew",
-        "-p",
-        "../api",
-        ":bootRun"
-    )
+tasks.run {
+    wrapper {
+        gradleVersion = "8.6"
+        distributionType = Wrapper.DistributionType.BIN
+    }
+    withType<JavaExec> {
+        jvmArgs = listOf(
+            "--add-modules=jdk.incubator.vector",
+            "--enable-native-access=ALL-UNNAMED",
+            "--enable-preview"
+        )
+    }
+    register<Exec>("reportTestApi") {
+        group = "api"
+        commandLine(
+            "./gradlew",
+            "-q",
+            "-s",
+            "-p",
+            "../api",
+            ":apiCheckFirefox"
+        )
+    }
+    register<Exec>("testApi") {
+        group = "api"
+        commandLine(
+            "./gradlew",
+            "-q",
+            "-s",
+            "-p",
+            "../api",
+            ":check",
+            "--rerun-tasks",
+        )
+    }
+    register<Exec>("runInstaller") {
+        group = "installer"
+        commandLine(
+            "java",
+            "-jar",
+            "../api/installer/build/libs/installer-${project.properties["artifact.version"]}.jar"
+        )
+    }
+    register<Exec>("runApi") {
+        group = "api"
+        commandLine(
+            "java",
+            "-jar",
+            "../api/build/libs/api-${project.properties["artifact.version"]}.jar"
+        )
+    }
+    register<Exec>("runLocalApi") {
+        group = "api"
+        commandLine(
+            "java",
+            "-D${School.SPRING_PROFILE_KEY}=local",
+            "-jar",
+            "../api/build/libs/api-${project.properties["artifact.version"]}.jar"
+        )
+    }
 }
 
 tasks.register<Exec>("runCli") {
     group = "api"
     commandLine(
         "./gradlew",
+        "-q",
+        "-s",
         "-p",
         "../api",
         ":cli"
-    )
-}
-
-tasks.register<Exec>("runInstaller") {
-    group = "installer"
-    commandLine(
-        "./gradlew",
-        "-p",
-        "../api",
-        ":runWorkspaceInstaller"
     )
 }
